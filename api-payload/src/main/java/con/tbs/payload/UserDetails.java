7f6fd452aacedd7d88b344900e2b427718a27809
angela <98644547+angelaxcm@users.noreply.github.com>
2022-06-07 02:52:27 +0800

    modified user details

--- api-payload/src/main/java/con/tbs/payload/UserDetails.java
+++ api-payload/src/main/java/con/tbs/payload/UserDetails.java
@@ -6,23 +6,23 @@ import java.time.LocalDateTime;
 
 @Data
 public class UserDetails {
-    private String mobileNumber;
+    private String userId;
     private String email;
     private String firstName;
     private String lastName;
     private Double balance;
     private LocalDateTime createdAt;
 
-    public UserDetails(String mobileNumber, String email, String firstName, String lastName, LocalDateTime createdAt) {
-        this.mobileNumber = mobileNumber;
+    public UserDetails(String userId, String email, String firstName, String lastName, LocalDateTime createdAt) {
+        this.userId = userId;
         this.email = email;
         this.firstName = firstName;
         this.lastName = lastName;
         this.createdAt = createdAt;
     }
 
-    public UserDetails(String mobileNumber, String email, String firstName, String lastName, Double balance, LocalDateTime createdAt) {
-        this.mobileNumber = mobileNumber;
+    public UserDetails(String userId, String email, String firstName, String lastName, Double balance, LocalDateTime createdAt) {
+        this.userId = userId;
         this.email = email;
         this.firstName = firstName;
         this.lastName = lastName;
